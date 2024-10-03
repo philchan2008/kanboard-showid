3a7d5b9782fcc06328738f2b2b4a60333e89e75d
phil <phil@flexsystem.com>
2024-10-03 10:24:32 +0100

    change of display ID format #KB[ID]####

--- Template/task/id_head.php
+++ Template/task/id_head.php
@@ -1,7 +1,7 @@
 <div id="id_sidebar_title"  class="HideID">
     <div class="sidebar-title">
         <h2><?=t('Task')?><span style="color: gray; ">
-        #<?=$task['id']?>
+        🆔<?=$task['id']?>
         </span></h2>
     </div>
 </div>
@@ -9,7 +9,7 @@
 <div id="id_task_title"  class="HideID">
     <div id="id_task_head"  class="ShowID">
         <div style="color: gray; ">
-        <font color="pink">K</font><font color="lightgray">B</font>#<?=$task['id']?>: <font color="black"><?=$task['title']?></font>
+        #<font color="pink">K</font><font color="lightgray">B</font>🆔<?=$task['id']?>: <font color="black"><?=$task['title']?></font>
         </div>
     </div>
 </div>
@@ -21,9 +21,9 @@
 </div>
 
 <div id="id_comment_title"  class="HideID">
-<span style="color: gray; ">🆔KB#<?=$task['id']?> 💬$comment_id</span>
+<span style="color: gray; ">#KB🆔<?=$task['id']?> #💬$comment_id</span>
 </div>
 
 <span id="id_subtask_title"  class="HideID">
-<span class="subtask_id">🆔KB#<?=$task['id']?>🔖$subtask_id</i></span>
+<span class="subtask_id">#KB🆔<?=$task['id']?> #🔖$subtask_id</i></span>
 </span>
--- Template/task/id_top.php
+++ Template/task/id_top.php
@@ -1,5 +1,5 @@
 <!-- <div id="id_top_right"  class="ShowID">
     <div style="color: gray; ">
-    <font color="pink">K</font><font color="lightgray">B</font>#<?=$task['id']?>
+    #<font color="pink">K</font><font color="lightgray">B</font>🆔<?=$task['id']?>
     </div>
 </div> -->

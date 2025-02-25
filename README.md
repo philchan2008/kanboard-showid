6fa5947e89f5bdd255d580d76c9874c81ee507cc
phil <phil@flexsystem.com>
2025-02-25 06:49:13 +0000

    - remove [ID] emoji

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Show task  and comment ID in Kanboard task details page.
 
 e.g.
 ```
-🆔KB#1234 💬3157 EditorName Created at: 2022-12-14 12:58 Updated at: 2022-12-20 17:12
+KB#1234 💬3157 EditorName Created at: 2022-12-14 12:58 Updated at: 2022-12-20 17:12
 
 Comment details
 ```
--- Template/task/id_head.php
+++ Template/task/id_head.php
@@ -1,7 +1,7 @@
 <div id="id_sidebar_title"  class="HideID">
     <div class="sidebar-title">
         <h2><?=t('Task')?><span style="color: gray; ">
-        🆔<?=$task['id']?>
+        <?=$task['id']?>
         </span></h2>
     </div>
 </div>
@@ -9,21 +9,21 @@
 <div id="id_task_title"  class="HideID">
     <div id="id_task_head"  class="ShowID">
         <div style="color: gray; ">
-        <font color="pink">K</font><font color="lightgray">B</font>🆔<?=$task['id']?>: <font color="black"><?=$task['title']?></font>
+        <font color="pink">K</font><font color="lightgray">B</font><?=$task['id']?>: <font color="black"><?=$task['title']?></font>
         </div>
     </div>
 </div>
 
 <div id="id_sidebar_head"  class="HideID">
     <div style="color: gray; ">
-    <font color="pink">K</font><font color="lightgray">B</font>🆔<?=$task['id']?>
+    <font color="pink">K</font><font color="lightgray">B</font><?=$task['id']?>
     </div>
 </div>
 
 <div id="id_comment_title"  class="HideID">
-<span style="color: gray; "><font color="pink">K</font><font color="lightgray">B</font>🆔<?=$task['id']?>💬$comment_id</span>
+<span style="color: gray; "><font color="pink">K</font><font color="lightgray">B</font><?=$task['id']?>💬$comment_id</span>
 </div>
 
 <span id="id_subtask_title"  class="HideID">
-<span class="subtask_id"><font color="pink">K</font><font color="lightgray">B</font>🆔<?=$task['id']?>🔖$subtask_id</i></span>
+<span class="subtask_id"><font color="pink">K</font><font color="lightgray">B</font><?=$task['id']?>🔖$subtask_id</i></span>
 </span>
